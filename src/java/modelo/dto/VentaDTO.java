ee19486c0581219c0495a3d03cdd046f47aff8c4
Brian Pineda <brapinedao@correo.udistrital.edu.co>
2021-10-14 00:23:23 -0500

    Creando el archivo DTO con sus respectivas clases

---
+++ src/java/modelo/dto/VentaDTO.java
@@ -0,0 +1,132 @@
+package modelo.dto;
+
+
+public class VentaDTO {
+    int id;
+    int item;
+    int idCliente;
+    int idEmpleado;
+    int idProducto;
+    String numSerie;
+    String descripcionP;
+    String fecha;
+    int cantidad;
+    double subTotal;
+    double precio;
+    String estado;
+
+    public VentaDTO() {
+    }
+
+    public VentaDTO(int id, int item, int idCliente, int idEmpleado, int idProducto, String numSerie, String descripcionP, String fecha, int cantidad, double subTotal, double precio, String estado) {
+        this.id = id;
+        this.item = item;
+        this.idCliente = idCliente;
+        this.idEmpleado = idEmpleado;
+        this.idProducto = idProducto;
+        this.numSerie = numSerie;
+        this.descripcionP = descripcionP;
+        this.fecha = fecha;
+        this.cantidad = cantidad;
+        this.subTotal = subTotal;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getItem() {
+        return item;
+    }
+
+    public void setItem(int item) {
+        this.item = item;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(String numSerie) {
+        this.numSerie = numSerie;
+    }
+
+    public String getDescripcionP() {
+        return descripcionP;
+    }
+
+    public void setDescripcionP(String descripcionP) {
+        this.descripcionP = descripcionP;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(double subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+}
